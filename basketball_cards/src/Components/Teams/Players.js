24bc6775d3a729514cc63b1e63f4a40cd8f16832
Khalil <khalilfoulks@gmail.com>
2021-03-08 17:01:04 -0500

    removed store

--- basketball_cards/src/Components/Teams/Players.js
+++ basketball_cards/src/Components/Teams/Players.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
-function Players() {
+function Players(props) {
+    const { playerInfo } = props;
+
     return (
         <div>
             <h2>Players Page</h2>
+            <div>{playerInfo.first_name} {playerInfo.last_name}</div>
+            <div>{playerInfo.team.full_name}</div>
         </div>
     )
 }
