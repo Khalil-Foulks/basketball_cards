441eec962a4dd13e073e9cf09532db74ac805f4c
Khalil <khalilfoulks@gmail.com>
2021-03-08 16:59:58 -0500

    removed store and connected to API

--- basketball_cards/src/Components/Teams/TeamPage.js
+++ basketball_cards/src/Components/Teams/TeamPage.js
@@ -1,17 +1,18 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useState } from 'react';
 import Players from './Players';
 import axios from 'axios';
-import { ContextPlayerData } from '../Store'
 
 function TeamPage() {
-    // const [playerData, setPlayerData] = useContext()
+    const [players, setPlayers] = useState([]);
+    const [teamName, setTeamName] = useState('');
+    
     const baseUrl = 'https://www.balldontlie.io/api/v1/players'
 
     useEffect(() => {
         axios.get(baseUrl + '?page=0&per_page=100')
         .then(res => {
-            console.log(res.data)
-            // setPlayerData(res.data);
+            console.log(res.data.data)
+            setPlayers(res.data.data)
         })
         .catch(err => {
             console.log(err);
@@ -21,7 +22,13 @@ function TeamPage() {
     return (
         <div>
             <h2>TeamPage</h2>
-            <Players/>
+            {
+                players.filter((playerInfo) => {
+                    return playerInfo.team.full_name === 'Indiana Pacers';
+                }).map((playerInfo) => (
+                    <Players key={playerInfo.id} playerInfo={playerInfo}/>
+                ))
+            }
         </div>
     )
 }
