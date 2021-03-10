5d625c9d93bf3959cf71797ffd9c2640ed7c1588
Khalil <khalilfoulks@gmail.com>
2021-03-10 16:07:54 -0500

    added dropdown menu

--- basketball_cards/src/App.js
+++ basketball_cards/src/App.js
@@ -5,7 +5,6 @@ import TeamPage from './Components/Teams/TeamPage'
 function App() {
   return (
     <div className="App">
-      <h1>TESTING FROM App.js</h1>
       <TeamPage/>
     </div>
   );
--- basketball_cards/src/Components/Teams/TeamPage.js
+++ basketball_cards/src/Components/Teams/TeamPage.js
@@ -4,7 +4,15 @@ import axios from 'axios';
 
 function TeamPage() {
     const [players, setPlayers] = useState([]);
-    const [teamName, setTeamName] = useState('');
+    const [teamName, setTeamName] = useState(['Atlanta Hawks','Boston Celtics','Brooklyn Nets','Charlotte Hornets','Chicago Bulls','Cleveland Cavaliers','Dallas Mavericks','Denver Nuggets','Detroit Pistons','Golden State Warriors','Houston Rockets','Indiana Pacers','LA Clippers','Los Angeles Lakers','Memphis Grizzlies','Miami Heat','Milwaukee Bucks','Minnesota Timberwolves','New Orleans Pelicans','New York Knicks','Oklahoma City Thunder','Orlando Magic','Philadelphia 76ers','Phoenix Suns','Portland Trail Blazers','Sacramento Kings','San Antonio Spurs','Toronto Raptors','Utah Jazz','Washington Wizards']);
+
+    const [value, setValue] = useState(teamName[0])
+
+    const Add = teamName.map(Add => Add)
+    const handleClick = (e) => {
+        setValue(teamName[e.target.value])
+        console.log(value)
+    }
 
     const baseUrl = 'https://www.balldontlie.io/api/v1/players'
 
@@ -21,11 +29,23 @@ function TeamPage() {
 
     return (
         <div>
-            <h2>TeamPage</h2>
-            <h3>Players Page</h3>
+            <h2>{value} Page</h2>
+            <select
+                onChange={e => handleClick(e)}
+                className="Team_Select"
+            >
+                {
+                    Add.map((team, key) => 
+                        <option key={key} value={key}>
+                            {team}
+                        </option>
+                    )
+                }
+            </select>
+
             {
                 players.filter((playerInfo) => {
-                    return playerInfo.team.full_name === 'Indiana Pacers';
+                    return playerInfo.team.full_name === value;
                 }).map((playerInfo) => (
                     <Players key={playerInfo.id} playerInfo={playerInfo}/>
                 ))
